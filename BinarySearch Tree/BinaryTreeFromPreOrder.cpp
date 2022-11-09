d5506101e7a690c025443e17d2ae4ba0579eb533
masarath <zmasarath@gmail.com>
2022-11-09 17:05:49 +0530

    bst medium/hard

---
+++ BinarySearch Tree/BinaryTreeFromPreOrder.cpp
@@ -0,0 +1,87 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+#include <stdio.h>
+#include <stdlib.h>
+using namespace std;
+
+struct Node
+{
+	int data;
+	struct Node *left;
+	struct Node *right;
+	
+	Node(int x){
+	    data = x;
+	    left = NULL;
+	    right = NULL;
+	}
+};
+
+void printInorder (struct Node* node)
+{
+	if (node == NULL)return;
+	printInorder(node->left);
+	printf("%d ", node->data);
+	printInorder (node->right);
+}
+
+Node* constructTree(int n, int pre[], char preLN[]);
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        Node* root = NULL;
+        int n;
+        cin>>n;
+        int pre[n];
+        char preLN[n];
+        for(int i=0; i<n; i++)cin>>pre[i];
+        for(int i=0; i<n; i++)cin>>preLN[i];
+        root = constructTree (n, pre, preLN);
+        printInorder(root);
+        cout<<endl;
+    }
+	return 0;
+}
+
+// } Driver Code Ends
+
+
+/*Structure of the Node of the binary tree is as
+struct Node
+{
+	int data;
+	struct Node *left;
+	struct Node *right;
+};
+*/
+// function should return the root of the new binary tree formed
+struct Node *solve(int pre[],char preLN[],int &i,int n){
+    if(i>n){
+       return NULL;
+    }
+    
+    if(preLN[i]=='L'){
+        return new Node(pre[i++]);
+    }
+    struct Node *root= new struct Node(pre[i++]);
+    
+    root->left= solve(pre,preLN,i,n);
+    root->right = solve(pre,preLN,i,n);
+    
+    return root;
+    
+}
+struct Node *constructTree(int n, int pre[], char preLN[])
+{
+   
+    int i=0;
+    return solve(pre,preLN,i,n);
+}
+/*Construct a binary tree of size N using two given arrays pre[] and preLN[]. 
+Array pre[] represents preorder traversal of a binary tree. Array preLN[] 
+has only two possible values L and N. The value L in preLN[] indicates that the corresponding node
+ in Binary Tree is a leaf node and value N indicates that the corresponding node is a non-leaf nod*/
