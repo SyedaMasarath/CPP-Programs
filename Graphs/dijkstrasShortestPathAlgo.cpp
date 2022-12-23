189910b7e4a65325c0058693081a67b0c356131f
masarath <zmasarath@gmail.com>
2022-12-23 11:29:52 +0530

    dijkstras algo shortest path

---
+++ Graphs/dijkstrasShortestPathAlgo.cpp
@@ -0,0 +1,95 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+	public:
+	//Function to find the shortest distance of all the vertices
+    //from the source vertex S.
+    vector <int> dijkstra(int V, vector<vector<int>> adj[], int S)
+    {
+        set<pair<int,int>> st;
+        vector<int> dist(V);
+        for(int i=0;i<V;i++){
+            dist[i]=INT_MAX;
+        }
+        dist[S]=0;
+        st.insert({0,S});
+        
+        //process all pairs in set
+        while(!st.empty()){
+            auto top= *(st.begin());
+            
+            int nodeDist=top.first;
+            int topNode=top.second;
+            
+            //remove top record
+            
+            st.erase(st.begin());
+            
+            //get neighbours
+            for(auto neighbour : adj[topNode]){
+                //neighbours weight in adj
+                if(nodeDist + neighbour[1] < dist[neighbour[0]]){
+                    //check if any other record exists for same node
+                    auto record = st.find(make_pair(dist[neighbour[0]],neighbour[0]));
+                    //if found erase
+                    if(record!=st.end()){
+                        st.erase(record);
+                    }
+                    
+                    dist[neighbour[0]]=nodeDist + neighbour[1];
+                    
+                    st.insert(make_pair(dist[neighbour[0]],neighbour[0]));
+                }
+            }
+        }
+        return dist;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--) {
+        int V, E;
+        cin >> V >> E;
+        vector<vector<int>> adj[V];
+        int i=0;
+        while (i++<E) {
+            int u, v, w;
+            cin >> u >> v >> w;
+            vector<int> t1,t2;
+            t1.push_back(v);
+            t1.push_back(w);
+            adj[u].push_back(t1);
+            t2.push_back(u);
+            t2.push_back(w);
+            adj[v].push_back(t2);
+        }
+        int S;
+        cin>>S;
+        
+        Solution obj;
+    	vector<int> res = obj.dijkstra(V, adj, S);
+    	
+    	for(int i=0; i<V; i++)
+    	    cout<<res[i]<<" ";
+    	cout<<endl;
+    }
+
+    return 0;
+}
+
+
+// } Driver Code Ends
+//ip undirected graph
+//nodes with weights
+// op : dist of every vertex from source
